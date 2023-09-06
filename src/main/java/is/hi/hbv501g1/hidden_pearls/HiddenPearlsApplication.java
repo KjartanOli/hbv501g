00e6fa516cae79868996bc8ae7c41b741461a8db
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-09-06 13:50:02 +0000

    Initial commit

---
+++ src/main/java/is/hi/hbv501g1/hidden_pearls/HiddenPearlsApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g1.hidden_pearls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiddenPearlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiddenPearlsApplication.class, args);
+	}
+
+}
