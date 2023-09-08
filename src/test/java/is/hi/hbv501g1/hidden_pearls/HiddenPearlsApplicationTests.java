18b3be59e0a71e5db9a1cec48f88785ebccd538a
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-09-08 13:39:24 +0000

    Add missing test file
    * src/test/java/is/hi/hbv501g1/hidden_pearls/HiddenPearlsApplicationTests.java:
      Add test file.

---
+++ src/test/java/is/hi/hbv501g1/hidden_pearls/HiddenPearlsApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g1.hidden_pearls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddenPearlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
