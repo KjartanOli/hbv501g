70f2a7c2ca5d1ce851fbcfb21758c2ac73f9eff5
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2023-10-26 13:21:18 +0000

    Add Spring Security
    * pom.xml (dependencies): Add spring-security.
    
    * src/main/java/is/hi/hbv501g1/hidden_pearls/HiddenPearlsApplication.java:
      Disable spring-security's default configuration.

--- src/main/java/is/hi/hbv501g1/hidden_pearls/HiddenPearlsApplication.java
+++ src/main/java/is/hi/hbv501g1/hidden_pearls/HiddenPearlsApplication.java
@@ -2,8 +2,11 @@ package is.hi.hbv501g1.hidden_pearls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// We only want spring-security for the PasswordEncoder classes all of
+// its default configuration needs to kindly go away.
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class HiddenPearlsApplication {
 
 	public static void main(String[] args) {
